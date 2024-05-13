edae362262f87ad6bb4b0b5b2db68b6ea13e5bf2
Diego Bomfim <diegobonfim@gmail.com>
2024-05-13 19:29:14 -0300

    initial commit

---
+++ frontend-sem-redux/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Diego Bomfim é um profissional com 3 graduações e sabe programar.</p>
+        <h2>Missão e Visão</h2>
+        <p>Ser um profissional multidisciplinar dominando conhecimentos de mercado financeiro e tecnologia</p>
+        <h2>Imprensa</h2>
+        <p>Acompanhem minhas redes sociais.</p>
+    </div>
+)
